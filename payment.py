28f69d9b6097d2db7646602e6c79e183d6a5064d
LongDE2003 <longlovers2003@gmail.com>
2023-11-03 07:42:48 +0700

    Learn Unit Testing

---
+++ payment.py
@@ -0,0 +1,13 @@
+from order import Order
+from processor import PaymentProcessor
+
+
+def pay_order(order: Order) -> None:
+    if order.total == 0:
+        raise ValueError("Can't pay an order with total 0.")
+    card = input("please enter your card number: ")
+    month = int(input("please enter the card expiry month: "))
+    year = int(input("please enter the card expiry year: "))
+    payment_processor = PaymentProcessor("")
+    payment_processor.charge(card, month, year, amount=order.total)
+    order.pay()
